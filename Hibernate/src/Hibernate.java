81bb573dd740e9c6901a434f538fcbbc7b16684d
Arias3601 <3601arias@gmail.com>
2016-09-26 13:39:19 -0600

    Update to make sure I have everything pushed that I have currently.

--- Hibernate/src/Hibernate.java
+++ Hibernate/src/Hibernate.java
@@ -19,10 +19,10 @@ public class Hibernate {
     public static void main(String[] args){
 
         Hibernate hereWeGo = new Hibernate();
-        //hereWeGo.newCharacters();
+        hereWeGo.newCharacters();
         hereWeGo.showCharacters();
-        //hereWeGo.modifyCharacter();
-        //hereWeGo.deleteCharacter();
+        hereWeGo.modifyCharacter();
+        hereWeGo.deleteCharacter();
     }
 
     private void newCharacters() {
@@ -42,19 +42,45 @@ public class Hibernate {
         Session session = happyUtility.getCurrentSession();
         Transaction transaction = session.beginTransaction();
 
-        //This is the SQL code (HAPPY)
-        Query allCharactersQuery = session.createQuery("select c from Character as c order by c.name");
-
         //NASTY
         //Order by something not in the DataBase - Couldn't find it so It threw an exception and didn't do the rest of the code
-        //Query allCharactersQuery = session.createQuery("select c from Character as c order by c.notReal");
+
+        try {
+            Query allCharactersQuery = session.createQuery("select c from Character as c order by c.notReal");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         //what if the query is an empty string? - States "unexpected end of subtree" and ends the program
-        //Query allCharactersQuery = session.createQuery("");
+        try {
+            Query allCharactersQuery = session.createQuery("");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        //hello world, null, put a bunch of random text after the HQL begining
+        //Hello world - unexpected token Hello so it doesn't work, breaks code despite being in a try catch
+      /*  try {
+            Query allCharactersQuery = session.createQuery("Hello World");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
 
-        //put everything in main
+        //null - states that it is ambiguous and prevents the code from working at all. (despite the try catch)
+       /* try {
+            Query allCharactersQuery = session.createQuery(null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+
+        //begining is HQL end is not - I causes the error not but, but it still doesn't work, illeagel token
+        try {
+            Query allCharactersQuery = session.createQuery("select c from Character but I dont understand this");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //This is the SQL code (HAPPY)
+        Query allCharactersQuery = session.createQuery("select c from Character as c order by c.name");
 
         characters = allCharactersQuery.list();
         for (int i = 0; i < characters.size(); i++) {
@@ -68,25 +94,29 @@ public class Hibernate {
         Session session = happyUtility.getCurrentSession();
         Transaction transaction = session.beginTransaction();
 
+        //NASTY
+        //What if the where statement isn't correct? (that person doesn't exisit in the DB) - It sets the singleCharacterQuery to null so it fails
+
+        try {
+            Query singleCharacterQuery = session.createQuery("select c from Character as c where c.name='Joe'");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //What if there is no where clause? - the query does not return a uniqe result and therefore cannot work
+        try {
+            Query singleCharacterQuery = session.createQuery("select c from Character as c");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         //This is the HQL code (HAPPY)
         Query singleCharacterQuery = session.createQuery("select c from Character as c where c.name='Mario'");
 
-        //NASTY
-        //What if the where statement isn't correct? (that person doesn't exisit in the DB) - It sets the singleCharacterQuery to null so it fails
-        //Query singleCharacterQuery = session.createQuery("select c from Character as c where c.name='Joe'");
-
-        //What if there is no where clause? - the query does not return a uniqe result and therefore cannot work
-        //Query singleCharacterQuery = session.createQuery("select c from Character as c");
-
-        Character mario = (Character)singleCharacterQuery.uniqueResult();
-
-        mario.setName("Wario");
-
-        session.merge(mario);
-
+        Character beforeName = (Character)singleCharacterQuery.uniqueResult();
+        beforeName.setName("Wario");
+        session.merge(beforeName);
         transaction.commit();
-
-        showCharacters();
     }
 
     private void deleteCharacter(){
@@ -161,69 +191,132 @@ class ConfigH {
 
     public ConfigH() {
         Configuration config = new Configuration();
-        //HAPPY DIALECT
-        config.setProperty("hibernat.dialect", "org.hibernate.dialect.MYSQLDialect");
-
         //NASTY DIALECT org.hibernate.dialect
         //using Oracle Dialect - It worked... It seems that sense the rest of the hibernate code was for MySQL it was still able to work
-        //config.setProperty("hibernat.dialect", "org.hibernate.dialect.Oracle10gDialect");
+        try {
+            config.setProperty("hibernat.dialect", "org.hibernate.dialect.Oracle10gDialect");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         //Empty String for Dialect - Again it worked, seems to figure it out itself.
-        //config.setProperty("hibernat.dialect", "");
+        try {
+            config.setProperty("hibernat.dialect", "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         //No hibernat.dialect property - Yep it still worked, go figure!
 
+        //HAPPY DIALECT
+        config.setProperty("hibernat.dialect", "org.hibernate.dialect.MYSQLDialect");
+
+        //NASTY jdbc driver
+        //the driver portion is not set (empty string) - no driver specified so it didn't work
+        try {
+            config.setProperty("hibernate.connection.driver_class", "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //using the oracle driver - couldn't find the driver, but it did try despite being told to use the MySQL dialect earlier
+        try {
+            config.setProperty("hibernate.connection.driver_class", "oracle.jdbc.driver.OracleDriver");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         //HAPPY jdbc driver
         config.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver");
 
-        //NASTY jdbc driver
-        //the driver portion is not set (empty string) - no driver specified so it didn't work
-        //config.setProperty("hibernate.connection.driver_class", "");
 
-        //using the oracle driver - couldn't find the driver, but it did try despite being told to use the MySQL dialect earlier
-        //config.setProperty("hibernate.connection.driver_class", "oracle.jdbc.driver.OracleDriver");
+        //NASTY url
+        //wrong database, but the database exisists - the table characters doesn't exisist in test so it fails
+        try {
+            config.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/test");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+        //the database doesn't exisist - Unknown database so it fails
+        try {
+            config.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/noDataBase");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //the port isn't used for the database - connecting to the MySQL database in general fails, so it fails
+        try {
+            config.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:7777/Java");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //empty string for the url - unable to make a jdbc connection do to no url
+        try {
+            config.setProperty("hibernate.connection.url", "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //Null - null pointer eception
+        try {
+            config.setProperty("hibernate.connection.url", null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            config.setProperty("hibernate.connection.url", "Hello World");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        try {
+            config.setProperty("hibernate.connection.url", "http:mysql://localhost:3306/noDataBase");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         //HAPPY url
         config.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/Java");
 
-        //NASTY url
-        //wrong database, but the database exisists - the table characters doesn't exisist in test so it fails
-        //config.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/test");
 
-        //the database doesn't exisist - Unknown database so it fails
-        //config.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/noDataBase");
+        //NASTY username
+        //use a fake username - access denied
+        try {
+            config.setProperty("hibernate.connection.username", "notReal");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        //the port isn't used for the database - connecting to the MySQL database in general fails, so it fails
-        //config.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:7777/Java");
-
-        //empty string for the url - unable to make a jdbc connection do to no url
-        //config.setProperty("hibernate.connection.url", "");
-
-
-        //null or HTTP protocol, Hello World
+        //use null for the username - It before even attempting as there is a NullPointerException
+        try {
+            config.setProperty("hibernate.connection.username", null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         //HAPPY username
         config.setProperty("hibernate.connection.username", "purplesp_iUser");
 
-        //NASTY username
-        //use a fake username - access denied
-        //config.setProperty("hibernate.connection.username", "notReal");
+        //Nasty password
+        //using a fake password - access denied
+        try {
+            config.setProperty("hibernate.connection.password", "fakePassword");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-        //use null for the username - It before even attempting as there is a NullPointerException
-        //config.setProperty("hibernate.connection.username", null);
-
+        //use null for the password - It before even attempting as there is a NullPointerException
+        try {
+            config.setProperty("hibernate.connection.password", null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         //HAPPY password
         config.setProperty("hibernate.connection.password", "RXZfwSvAQCSf");
-
-        //Nasty password
-        //using a fake password - access denied
-        //config.setProperty("hibernate.connection.password", "fakePassword");
-
-        //use null for the password - It before even attempting as there is a NullPointerException
-        //config.setProperty("hibernate.connection.password", null);
 
         config.setProperty("hibernate.connection.pool_size", "10");
         config.setProperty("hibernate.connection.autocommit", "true");
